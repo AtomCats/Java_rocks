aab7f1b44bf2cacb4170ba90ffd3923369eb346f
AtomCats <toni-e@yandex.ru>
2016-06-28 12:39:37 +0300

    Working version 1

--- src/com/tasks/first/First.java
+++ src/com/tasks/first/First.java
@@ -3,14 +3,19 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import  java.util.Scanner;
 
 
 public class First {
     public void greetings() {
+        System.out.println("Error!!!!!I'm out!");
 
-        System.out.println("Sup dude?");
-        System.out.println("Dude,type some integers to calculate their sum");
-
+    }
+    public int greetings(int first,int second)
+    {
+        return (first+second);
     }
 }
--- src/com/tasks/first/Main.java
+++ src/com/tasks/first/Main.java
@@ -1,23 +1,31 @@
 package com.tasks.first;
 
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Created by Admin on 27.06.2016.
  */
 public class Main {
     public static void main(String[ ] args)
-    {
-
-        First task = new First();
+    {   First task = new First();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sup dude?");
+        System.out.println("Dude,type some integers to calculate their sum \n Type first integer:");
         try {
-            task.greetings();
-            }
-        catch (IOException e)
-        {
-            System.out.println("Suck!");
+            int first = sc.nextInt();
+            System.out.println("Good boy) \n And type second integer");
+            int sec = sc.nextInt();
+            int res = task.greetings(first, sec);
+            System.out.println("Here is your sum:" + res);
+        }
+        catch (InputMismatchException e) {
+        task.greetings();
         }
     }
 }
